7df0ce003a4c1e7d0f1e0f8cdee54c697f06c33c
IdanLnrd <87257934+IdanLnrd@users.noreply.github.com>
2021-11-25 11:59:24 +0200

    routeing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
 import './App.css';
-
+import {
+  Alignment,
+  AnchorButton,
+  Classes,
+  Navbar,
+  NavbarDivider,
+  NavbarGroup,
+  NavbarHeading,
+} from "@blueprintjs/core";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ShapesEditor from './components/ShapesEditor';
 function App() {
-
   return (
     <div className="App">
-        
+         <Navbar>
+              <NavbarGroup align={Alignment.LEFT}>
+                  <NavbarHeading>IG</NavbarHeading>
+                  <NavbarDivider />
+                  <AnchorButton className={Classes.MINIMAL} href="/" icon="home" text="" />
+                  <NavbarDivider />
+                  <AnchorButton className={Classes.MINIMAL} href="/shapes" icon="shapes" text="Shapes" />
+              </NavbarGroup>
+          </Navbar>
+          <BrowserRouter>
+              <Routes>
+                  <Route path="/shapes" element={<ShapesEditor />} />
+              </Routes>
+          </BrowserRouter>
     </div>
   );
 }
--- src/components/ShapesEditor/index.tsx
+++ src/components/ShapesEditor/index.tsx
@@ -4,18 +4,13 @@ import { schema } from '../Shapes/shapes';
 import { ShapeOfTheRoleLayer } from '../Shapes/types';
 import { useEffect, useState } from 'react';
 
-const DEF = [
-  { scale: 1, label: 'l1', opacity: 1 },
-  { scale: 0.95, label: 'l2', motion: true },
-  { scale: 0.85, label: 'l3', motion: true },
-];
 const ShapesEditor: React.FunctionComponent<{ 
-    title: string, storeLocally?: boolean 
+    title?: string, storeLocally?: boolean 
 }> = ({ title, storeLocally }) => {
 
     const [data, setData] = useState<ShapeOfTheRoleLayer[]>(() => {
         const d = JSON.parse(localStorage.getItem('shapes') || "[]");
-        return d || DEF;
+        return d;
     });
 
     useEffect(() => {
@@ -25,7 +20,7 @@ const ShapesEditor: React.FunctionComponent<{
     }, [ data, storeLocally ]);
 
     return <div>
-      <h1>Shape of the role</h1>
+      <h1>{ title }</h1>
       <ShapeOfTheRole layers={data} />
       <JsonForm 
         data={data} 
