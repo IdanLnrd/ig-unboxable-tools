4d6bcb08150cf867258acedb50a47f7965948ae9
IdanLnrd <87257934+IdanLnrd@users.noreply.github.com>
2021-11-26 14:23:32 +0200

    home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,19 @@
-import { H1 } from "@blueprintjs/core";
+import { Classes, H1 } from "@blueprintjs/core";
 import "./home.css";
 
 const Home: React.FunctionComponent = () => {
     return <div>
         <H1 className="home-title">Home</H1>
+        <p className={`${Classes.TEXT_MUTED} ${Classes.TEXT_LARGE} ${Classes.RUNNING_TEXT} home-desc`}>
+            <section>
+            <span>
+                This site goal is reducing the amount of human contact and automated the solutions for some problems.
+            </span>
+            </section>
+            <section>
+                <span className={`${Classes.TEXT_SMALL}`}>Idan Gibly</span>
+            </section>
+        </p>
     </div>
 }
 
