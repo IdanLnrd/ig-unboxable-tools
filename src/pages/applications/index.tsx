9433dc25fcd29b514b905e6039c3d551a0b2015c
IdanLnrd <87257934+IdanLnrd@users.noreply.github.com>
2021-11-26 08:32:46 +0200

    adding more apps

--- src/pages/applications/index.tsx
+++ src/pages/applications/index.tsx
@@ -8,11 +8,25 @@ const Applications: FunctionComponent = () => {
     return <div>
         <H1 className="apps-title">Applications</H1>
         <div className="apps">
-            <Card>
+            <Card className="app-item">
                 <AnchorButton 
-                    text="Unboxable Shapes" 
+                    text="Shapes" 
                     href="/applications/shapes"
                     icon="cube" className={Classes.MINIMAL} /> 
+            </Card>
+            <Card className="app-item">
+                <AnchorButton 
+                    text="Editor" 
+                    target="_blank"
+                    href="https://editor.lnrd.io"
+                    icon="edit" className={Classes.MINIMAL} /> 
+            </Card>
+            <Card className="app-item">
+                <AnchorButton 
+                    text="Analytics" 
+                    target="_blank"
+                    href="https://analytics.staging.lnrd.io"
+                    icon="chart" className={Classes.MINIMAL} /> 
             </Card>
         </div>
     </div>
